0183c52d9ded9e7795b57025fa453666879f6ff5
KRISH DHIMAN <krrishdhiman841@gmail.com>
2024-11-06 18:47:03 +0530

    update

--- backend/connection/mongodb.connection.js
+++ backend/connection/mongodb.connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/food_delivery')
+
+
+mongoose.connect('mongodb+srv://krrishdhiman841:cVPJenu79CI15nXm@cluster0.mongodb.net/food_delivery?retryWrites=true&w=majority')
     .then(() => console.log("monogDB connected!"))
     .catch(() => console.log("Error connecting to MongoDB"))
-    
+
 module.exports = mongoose;
