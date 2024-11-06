2815d283a0bdda0ad665f8bd42c85c9bbac57c4e
KRISH DHIMAN <krrishdhiman841@gmail.com>
2024-11-06 16:55:05 +0530

    changes

--- backend/connection/mongodb.connection.js
+++ backend/connection/mongodb.connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb://localhost:27017/food_delivery',).then(() => {
+mongoose.connect('mongodb://localhost:27017/food_delivery').then(() => {
     console.log("monogDB connected!")
 }).catch(() => {
     console.log("Error connecting to MongoDB")
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider, Routes,Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import AppLayout from './AppLayout'
 import Contact from './pages/Contact'
